b9e06de4237579a6fc7487d650466a5460cb3e4d
Atsu <zatsuyou0@gmail.com>
2018-11-15 21:06:21 +0900

    一覧で出てこない

--- Unitus/xcode/soccer_list/soccer_list/CustomCell.swift
+++ Unitus/xcode/soccer_list/soccer_list/CustomCell.swift
@@ -15,7 +15,16 @@ class CustomCell: UITableViewCell {
     @IBOutlet weak var teame_name: UILabel!
     @IBOutlet weak var reaage: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
     
-    
+//    override func setSelected(selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//        
+//        // Configure the view for the selected state
+//    }
+//    
     
 }
--- Unitus/xcode/soccer_list/soccer_list/TableViewController.swift
+++ Unitus/xcode/soccer_list/soccer_list/TableViewController.swift
@@ -9,24 +9,46 @@
 import UIKit
 
 class TableViewController : UITableViewController  {
+    
+    /// 画像のファイル名
+    let imageNames = ["バルセロナ.jpg", "ナポリ.jpg", "リアルマドリード.jpg", "ガンバ大阪.jpg"]
+    
+    /// 画像のタイトル
+    let imageTitles = ["バルセロナ", "ナポリ", "リアルマドリード", "ガンバ大阪"]
+    
+    /// 画像の説明
+    let imageDescriptions = [
+        "ああああああああああああ",
+        "ああああああああああああ",
+        "ああああああああああああ",
+        "ああああああああああああ"
+    ]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        //これ何？
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     /// セルの個数を指定するデリゲートメソッド（必須）
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return imageNames.count
     }
     
     /// セルに値を設定するデータソースメソッド（必須）
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView as! CustomCell
+        //？
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell")  as! CustomCell
         
         // セルに値を設定
         cell.soccer_image.image = UIImage(named: imageNames[indexPath.row])
